ecb0a25baa8cca5ec7628542b3a13d2a461fa252
Mohit Kumar <133571577+mohitaryan0@users.noreply.github.com>
2025-05-15 02:35:01 +0530

    fixed the errors

--- src/components/top/top.tsx
+++ src/components/top/top.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import AnimatedText from "../ui/animated-text";
 import WordByWordText from "../ui/word-by-word-text";
 
